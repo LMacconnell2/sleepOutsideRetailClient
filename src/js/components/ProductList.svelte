05d146155f68d5d1d6ca7955b2bd5fe3e9c343e0
hyrum james bullock <hyrumbullock@gmail.com>
2026-03-03 10:32:01 -0700

    added discount markers to listcards

--- src/js/components/ProductList.svelte
+++ src/js/components/ProductList.svelte
@@ -1,34 +1,52 @@
-<script lang=ts>
-    import { onMount } from "svelte";
-    import { getParam } from '../utils.mts';
-    import { getProductData } from '../productData.mts';
-    import type { Product } from '../types.mts';
+<script lang="ts">
+  import { onMount } from "svelte";
+  import { getParam } from "../utils.mts";
+  import { getProductData } from "../productData.mts";
+  import type { Product } from "../types.mts";
+  import { calcDiscount } from "../../js/pricing.ts";
 
-    let category = $state("");
-    let products:Product[] = $state([]);
-    async function init() {
-        category = getParam("category") || "";
-        const data = await getProductData(category);
-        products = data.results;
-    }
+  let category = $state("");
+  let products: Product[] = $state([]);
+  async function init() {
+    category = getParam("category") || "";
+    const data = await getProductData(category);
+    products = data.results;
+  }
 
-    onMount(init);
+  // const discount = calcDiscount
+  // product.data.suggestedRetailPrice,
+  // product.data.finalPrice,
+  // const title = product.data.name
+
+  onMount(init);
 </script>
 
 <h2>Top Products: {category}</h2>
 
 <ul class="product-list">
-    {#each products as product}
-        <li class="product-card">
-            <a href='/products/{product.id}/'>
-                <img
-                src={product.images.primaryMedium}
-                alt="image of {product.name}"
-                />
-                <h3 class="card__brand">{product.brand.name}</h3>
-                <h2 class="card__name">{product.name}</h2>
-                <p class="product-card__price">{product.finalPrice}</p>
-            </a>
-        </li>
-    {/each}
+  {#each products as product}
+    {@const discount = calcDiscount(
+      product.suggestedRetailPrice,
+      product.finalPrice,
+    )}
+
+    <li class="product-card">
+      <a href="/products/{product.id}/">
+        <img src={product.images.primaryMedium} alt="image of {product.name}" />
+        <h3 class="card__brand">{product.brand.name}</h3>
+        <h2 class="card__name">{product.name}</h2>
+        <!-- <p class="product-card__price">{product.finalPrice}</p> -->
+        <p class="product-card__price">
+          {product.finalPrice}
+
+          <!-- if statement to show discount if there is one -->
+          {#if discount}
+            <span class="product__discount">
+              {discount.percent}% (<s>${product.suggestedRetailPrice}</s>)
+            </span>
+          {/if}
+        </p>
+      </a>
+    </li>
+  {/each}
 </ul>
